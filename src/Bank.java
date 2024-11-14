eab317bfc9d02eccf0626f1d731f434ef3e25850
annagillies1 <annagillies@hotmail.co.uk>
2024-11-14 16:13:06 +0000

    initial commit basic application

---
+++ src/Bank.java
@@ -0,0 +1,11 @@
+
+public class Bank {
+    public static void main(String[] args){
+
+        Account anna = new Account("Anna", "A0001");
+        anna.showMenu();
+
+        Account mark = new Account("Mark", "A0002");
+
+    }
+}
